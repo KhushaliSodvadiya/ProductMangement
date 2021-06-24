712c892b447c44a86c5e19edad97b84c3577faaf
Sharvari Thakar <Sharvari.Thakar@green-apex.com>
2021-06-24 10:42:25 +0530

    First commit

---
+++ src/app/components/sidebar/sidebar.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'sidebar',
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.css']
+})
+export class SidebarComponent implements OnInit {
+
+  constructor() { }
+  selectedList: string = "list"
+  ngOnInit(): void {
+  }
+
+  onListClick(value: string) {
+    debugger;
+    this.selectedList = value
+  }
+}
